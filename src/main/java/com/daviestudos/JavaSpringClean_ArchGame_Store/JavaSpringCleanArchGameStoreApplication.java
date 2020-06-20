b1bf6e421f5c376168ee3fff0056a5e2af836389
Davi Marques <daviestudos19@gmail.com>
2020-06-20 11:06:24 -0300

    Adicionando estrutura Spring boot

---
+++ src/main/java/com/daviestudos/JavaSpringClean_ArchGame_Store/JavaSpringCleanArchGameStoreApplication.java
@@ -0,0 +1,13 @@
+package com.daviestudos.JavaSpringClean_ArchGame_Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringCleanArchGameStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringCleanArchGameStoreApplication.class, args);
+	}
+
+}
